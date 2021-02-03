8a6ffa952bec0b745054603af331751043071be7
olexii <omedvedew@ukr.net>
2021-02-03 13:19:17 +0200

    added slider

--- app.js
+++ app.js
@@ -10,5 +10,46 @@ function showMobileMenu () {
 
 burgerBtn.addEventListener("click", showMobileMenu);
 
+// SLIDER-SCRIPT
+$(document).ready(function(){
+    $('.about-us-slider-container').slick({
+    //   setting-name: setting-value
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+              }
+            },
+            {
+              breakpoint: 765,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ],
+    });
+  });
+
+
+
+
+
 
 
